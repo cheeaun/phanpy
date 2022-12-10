33637c76d1c269112115133e1922cdf26668e855
Lim Chee Aun <cheeaun@gmail.com>
2022-12-10 20:48:09 +0800

    Let's change this and see if it's annoying

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -132,7 +132,7 @@ export default ({ hidden }) => {
               <Icon icon="notification" size="l" alt="Notifications" />
             </a>
           </div>
-          {snapStates.homeNew.length > 1 && (
+          {snapStates.homeNew.length > 0 && (
             <button
               class="updates-button"
               type="button"

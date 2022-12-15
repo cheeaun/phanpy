f7d2f6fdac764b7721b315b8da2ae8aa8c8d364c
Lim Chee Aun <cheeaun@gmail.com>
2022-12-15 11:47:11 +0800

    Enable source map and split vendor chunk

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import preact from '@preact/preset-vite';
 import { resolve } from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [preact(), splitVendorChunkPlugin()],
   build: {
+    sourcemap: true,
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),

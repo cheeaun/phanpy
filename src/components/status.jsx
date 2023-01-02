07dff34e20e0436a7ef6291f1a86f1032078556a
Lim Chee Aun <cheeaun@gmail.com>
2023-01-02 14:21:38 +0800

    Show formatted duration for video media

--- src/components/status.jsx
+++ src/components/status.jsx
@@ -764,9 +764,11 @@ function Media({ media, showOriginal, onClick = () => {} }) {
     const shortDuration = original.duration <= 20;
     const isGIF = type === 'gifv' || shortDuration;
     const loopable = original.duration <= 60;
+    const formattedDuration = formatDuration(original.duration);
     return (
       <div
         class={`media media-${isGIF ? 'gif' : 'video'}`}
+        data-formatted-duration={formattedDuration}
         style={{
           backgroundColor:
             rgbAverageColor && `rgb(${rgbAverageColor.join(',')})`,
@@ -1421,6 +1423,21 @@ function Carousel({ mediaAttachments, index = 0, onClose = () => {} }) {
       )}
     </>
   );
+}
+
+function formatDuration(time) {
+  if (!time) return;
+  let hours = Math.floor(time / 3600);
+  let minutes = Math.floor((time % 3600) / 60);
+  let seconds = Math.round(time % 60);
+
+  if (hours === 0) {
+    return `${minutes}:${seconds.toString().padStart(2, '0')}`;
+  } else {
+    return `${hours}:${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  }
 }
 
 export default Status;

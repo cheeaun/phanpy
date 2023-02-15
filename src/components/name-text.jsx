f1985713676a47a427fd492b41b31ac8f16d83ca
Lim Chee Aun <cheeaun@gmail.com>
2023-02-15 10:46:29 +0800

    Compare exact string before checking variants

--- src/components/name-text.jsx
+++ src/components/name-text.jsx
@@ -19,15 +19,16 @@ function NameText({
 
   const displayNameWithEmoji = emojifyText(displayName, emojis);
 
+  const trimmedUsername = username.toLowerCase().trim();
+  const trimmedDisplayName = (displayName || '').toLowerCase().trim();
+
   if (
-    !short &&
-    username.toLowerCase().trim() ===
-      (displayName || '')
+    (!short && trimmedUsername === trimmedDisplayName) ||
+    trimmedUsername ===
+      trimmedDisplayName
         .replace(/(\:(\w|\+|\-)+\:)(?=|[\!\.\?]|$)/g, '') // Remove shortcodes, regex from https://regex101.com/r/iE9uV0/1
         .replace(/\s+/g, '') // E.g. "My name" === "myname"
         .replace(/[^a-z0-9]/gi, '') // Remove non-alphanumeric characters
-        .toLowerCase()
-        .trim()
   ) {
     username = null;
   }

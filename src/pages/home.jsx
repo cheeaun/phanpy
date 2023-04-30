ef06faf259782cd7f82711415bb52e4555324083
Lim Chee Aun <cheeaun@gmail.com>
2023-04-30 21:03:09 +0800

    Notifications popover, for larger screens

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,20 @@
+import './notifications-menu.css';
+
+import { ControlledMenu } from '@szhsin/react-menu';
 import { memo } from 'preact/compat';
-import { useEffect } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import { useSnapshot } from 'valtio';
 
 import Columns from '../components/columns';
 import Icon from '../components/icon';
 import Link from '../components/link';
+import Loader from '../components/loader';
+import Notification from '../components/notification';
+import { api } from '../utils/api';
 import db from '../utils/db';
+import groupNotifications from '../utils/group-notifications';
 import openCompose from '../utils/open-compose';
-import states from '../utils/states';
+import states, { saveStatus } from '../utils/states';
 import { getCurrentAccountNS } from '../utils/store-utils';
 
 import Following from './following';
@@ -27,6 +34,9 @@ function Home() {
     })();
   }, []);
 
+  const notificationLinkRef = useRef();
+  const [menuState, setMenuState] = useState('closed');
+
   return (
     <>
       {(snapStates.settings.shortcutsColumnsMode ||
@@ -40,17 +50,31 @@ function Home() {
           id="home"
           headerStart={false}
           headerEnd={
-            <Link
-              to="/notifications"
-              class={`button plain notifications-button ${
-                snapStates.notificationsShowNew ? 'has-badge' : ''
-              }`}
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
-            >
-              <Icon icon="notification" size="l" alt="Notifications" />
-            </Link>
+            <>
+              <Link
+                ref={notificationLinkRef}
+                to="/notifications"
+                class={`button plain notifications-button ${
+                  snapStates.notificationsShowNew ? 'has-badge' : ''
+                } ${menuState}`}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  if (window.matchMedia('(min-width: calc(40em))').matches) {
+                    e.preventDefault();
+                    setMenuState((state) =>
+                      state === 'closed' ? 'open' : 'closed',
+                    );
+                  }
+                }}
+              >
+                <Icon icon="notification" size="l" alt="Notifications" />
+              </Link>
+              <NotificationsMenu
+                state={menuState}
+                anchorRef={notificationLinkRef}
+                onClose={() => setMenuState('closed')}
+              />
+            </>
           }
         />
       )}
@@ -73,6 +97,114 @@ function Home() {
         <Icon icon="quill" size="xl" alt="Compose" />
       </button>
     </>
+  );
+}
+
+const NOTIFICATIONS_LIMIT = 30;
+const NOTIFICATIONS_DISPLAY_LIMIT = 5;
+function NotificationsMenu({ anchorRef, state, onClose }) {
+  const { masto, instance } = api();
+  const snapStates = useSnapshot(states);
+  const [uiState, setUIState] = useState('default');
+
+  const notificationsIterator = masto.v1.notifications.list({
+    limit: NOTIFICATIONS_LIMIT,
+  });
+
+  async function fetchNotifications() {
+    const allNotifications = await notificationsIterator.next();
+    const notifications = allNotifications.value;
+
+    if (notifications?.length) {
+      notifications.forEach((notification) => {
+        saveStatus(notification.status, instance, {
+          skipThreading: true,
+        });
+      });
+
+      const groupedNotifications = groupNotifications(notifications);
+
+      states.notificationsLast = notifications[0];
+      states.notifications = groupedNotifications;
+    }
+
+    states.notificationsShowNew = false;
+    states.notificationsLastFetchTime = Date.now();
+    return allNotifications;
+  }
+
+  function loadNotifications() {
+    setUIState('loading');
+    (async () => {
+      try {
+        await fetchNotifications();
+        setUIState('default');
+      } catch (e) {
+        setUIState('error');
+      }
+    })();
+  }
+
+  useEffect(() => {
+    loadNotifications();
+  }, []);
+
+  return (
+    <ControlledMenu
+      menuClassName="notifications-menu"
+      state={state}
+      anchorRef={anchorRef}
+      onClose={onClose}
+      portal={{
+        target: document.body,
+      }}
+      overflow="auto"
+      viewScroll="close"
+      position="anchor"
+      align="center"
+      boundingBoxPadding="8 8 8 8"
+      unmountOnClose
+    >
+      <header>
+        <h2>Notifications</h2>
+      </header>
+      {snapStates.notifications.length ? (
+        <>
+          {snapStates.notifications
+            .slice(0, NOTIFICATIONS_DISPLAY_LIMIT)
+            .map((notification) => (
+              <Notification
+                key={notification.id}
+                instance={instance}
+                notification={notification}
+              />
+            ))}
+        </>
+      ) : uiState === 'loading' ? (
+        <div class="ui-state">
+          <Loader abrupt />
+        </div>
+      ) : (
+        uiState === 'error' && (
+          <div class="ui-state">
+            <p>Unable to fetch notifications.</p>
+            <p>
+              <button type="button" onClick={loadNotifications}>
+                Try again
+              </button>
+            </p>
+          </div>
+        )
+      )}
+      <footer>
+        <Link to="/mentions" class="button plain">
+          <Icon icon="at" /> <span>Mentions</span>
+        </Link>
+        <Link to="/notifications" class="button plain2">
+          <b>See all</b> <Icon icon="arrow-right" />
+        </Link>
+      </footer>
+    </ControlledMenu>
   );
 }
 

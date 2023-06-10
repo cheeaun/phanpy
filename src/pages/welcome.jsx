c99b97d20968ab9995f4addfc27df0d080bc2251
Lim Chee Aun <cheeaun@gmail.com>
2023-06-10 19:20:32 +0800

    Revamp welcome page

--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -5,6 +5,7 @@ import groupedNotificationsUrl from '../assets/features/grouped-notifications.jp
 import multiColumnUrl from '../assets/features/multi-column.jpg';
 import multiHashtagTimelineUrl from '../assets/features/multi-hashtag-timeline.jpg';
 import nestedCommentsThreadUrl from '../assets/features/nested-comments-thread.jpg';
+import logoText from '../assets/logo-text.svg';
 import logo from '../assets/logo.svg';
 import Link from '../components/link';
 import states from '../utils/states';
@@ -14,101 +15,88 @@ function Welcome() {
   useTitle(null, ['/', '/welcome']);
   return (
     <main id="welcome">
-      <h1>
-        <img
-          src={logo}
-          alt=""
-          width="24"
-          height="24"
-          style={{
-            aspectRatio: '1/1',
-          }}
-        />{' '}
-        Phanpy
-      </h1>
-      <h2>
-        Trunk-tastic
-        <br />
-        Mastodon Experience
-      </h2>
-      <p>A minimalistic opinionated Mastodon web client.</p>
-      <p>
-        <big>
-          <b>
-            <Link to="/login" class="button">
-              Log in
-            </Link>
-          </b>
-        </big>
-      </p>
-      <details id="why-container">
-        <summary>Why Phanpy?</summary>
+      <div class="hero-container">
+        <h1>
+          <img
+            src={logo}
+            alt=""
+            width="200"
+            height="200"
+            style={{
+              aspectRatio: '1/1',
+              marginBlockEnd: -16,
+            }}
+          />
+          <img src={logoText} alt="Phanpy" width="250" />
+        </h1>
+        <p>
+          <big>
+            <b>
+              <Link to="/login" class="button">
+                Log in
+              </Link>
+            </b>
+          </big>
+        </p>
+        <p class="desc">A minimalistic opinionated Mastodon web client.</p>
+      </div>
+      <div id="why-container">
         <div class="sections">
           <section>
-            <h4>Boosts Carousel</h4>
-            <p>
-              Visually separate original posts and re-shared posts (boosted
-              posts).
-            </p>
             <img
               src={boostsCarouselUrl}
               alt="Screenshot of Boosts Carousel"
               loading="lazy"
             />
+            <h4>Boosts Carousel</h4>
+            <p>
+              Visually separate original posts and re-shared posts (boosted
+              posts).
+            </p>
           </section>
           <section>
-            <h4>Nested comments thread</h4>
-            <p>Effortlessly follow conversations. Semi-collapsible replies.</p>
             <img
               src={nestedCommentsThreadUrl}
               alt="Screenshot of nested comments thread"
               loading="lazy"
             />
+            <h4>Nested comments thread</h4>
+            <p>Effortlessly follow conversations. Semi-collapsible replies.</p>
           </section>
           <section>
-            <h4>Grouped notifications</h4>
-            <p>
-              Similar notifications are grouped and collapsed to reduce clutter.
-            </p>
             <img
               src={groupedNotificationsUrl}
               alt="Screenshot of grouped notifications"
               loading="lazy"
             />
+            <h4>Grouped notifications</h4>
+            <p>
+              Similar notifications are grouped and collapsed to reduce clutter.
+            </p>
           </section>
           <section>
-            <h4>Single or multi-column</h4>
-            <p>
-              By default, single column for zen-mode seekers. Configurable
-              multi-column for power users.
-            </p>
             <img
               src={multiColumnUrl}
               alt="Screenshot of multi-column UI"
               loading="lazy"
             />
+            <h4>Single or multi-column</h4>
+            <p>
+              By default, single column for zen-mode seekers. Configurable
+              multi-column for power users.
+            </p>
           </section>
           <section>
-            <h4>Multi-hashtag timeline</h4>
-            <p>Up to 5 hashtags combined into a single timeline.</p>
             <img
               src={multiHashtagTimelineUrl}
               alt="Screenshot of multi-hashtag timeline with a form to add more hashtags"
               loading="lazy"
             />
+            <h4>Multi-hashtag timeline</h4>
+            <p>Up to 5 hashtags combined into a single timeline.</p>
           </section>
-          <p>Convinced yet?</p>
-          <p>
-            <big>
-              <b>
-                <Link to="/login" class="button">
-                  Log in
-                </Link>
-              </b>
-            </big>
-          </p>
         </div>
-      </details>
+      </div>
       <hr />
       <p>
         <a href="https://github.com/cheeaun/phanpy" target="_blank">

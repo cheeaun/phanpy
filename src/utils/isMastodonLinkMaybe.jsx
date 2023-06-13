db602147aba5d9e8cd1e24689653457ad8de1be8
Lim Chee Aun <cheeaun@gmail.com>
2023-06-13 21:08:59 +0800

    Handle GoToSocial links

--- src/utils/isMastodonLinkMaybe.jsx
+++ src/utils/isMastodonLinkMaybe.jsx
@@ -1,6 +1,8 @@
 export default function isMastodonLinkMaybe(url) {
   const { pathname } = new URL(url);
   return (
-    /^\/.*\/\d+$/i.test(pathname) || /^\/notes\/[a-z0-9]+$/i.test(pathname) // Misskey, Calckey
+    /^\/.*\/\d+$/i.test(pathname) ||
+    /^\/@[^/]+\/statuses\/\w+$/i.test(pathname) || // GoToSocial
+    /^\/notes\/[a-z0-9]+$/i.test(pathname) // Misskey, Calckey
   );
 }

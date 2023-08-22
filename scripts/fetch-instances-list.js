e88b24fe6fe06dde586d15b6eb2e54401b75df97
Lim Chee Aun <cheeaun@gmail.com>
2023-08-22 20:11:28 +0800

    Refresh instances list

--- scripts/fetch-instances-list.js
+++ scripts/fetch-instances-list.js
@@ -17,8 +17,18 @@ const results = await fetch(url, {
 });
 
 const json = await results.json();
+
+// Filters
+json.instances = json.instances.filter(
+  (instance) => Number(instance.connections) > 20,
+);
+
 const names = json.instances.map((instance) => instance.name);
 
 // Write to file
 const path = './src/data/instances.json';
 fs.writeFileSync(path, JSON.stringify(names, null, '\t'), 'utf8');
+
+// Write everything to file, for debugging
+const path2 = './src/data/instances-full.json';
+fs.writeFileSync(path2, JSON.stringify(json, null, '\t'), 'utf8');

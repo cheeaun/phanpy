b7416bc17d772c180cfc43585e4036126db16941
Lim Chee Aun <cheeaun@gmail.com>
2023-09-28 11:19:24 +0800

    Handle Takahe links

--- src/utils/isMastodonLinkMaybe.jsx
+++ src/utils/isMastodonLinkMaybe.jsx
@@ -2,7 +2,7 @@ export default function isMastodonLinkMaybe(url) {
   const { pathname } = new URL(url);
   return (
     /^\/.*\/\d+$/i.test(pathname) ||
-    /^\/@[^/]+\/statuses\/\w+$/i.test(pathname) || // GoToSocial
+    /^\/@[^/]+\/(statuses|posts)\/\w+\/?$/i.test(pathname) || // GoToSocial, Takahe
     /^\/notes\/[a-z0-9]+$/i.test(pathname) || // Misskey, Calckey
     /^\/(notice|objects)\/[a-z0-9-]+$/i.test(pathname) // Pleroma
   );

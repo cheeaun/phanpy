3721acf3d3eb26bc262eab6ba33c102fb1b90832
Lim Chee Aun <cheeaun@gmail.com>
2023-10-20 19:24:01 +0800

    Attempt to make month picker better

--- src/pages/account-statuses.jsx
+++ src/pages/account-statuses.jsx
@@ -16,6 +16,8 @@ import { saveStatus } from '../utils/states';
 import useTitle from '../utils/useTitle';
 
 const LIMIT = 20;
+const MIN_YEAR = 1983;
+const MIN_YEAR_MONTH = `${MIN_YEAR}-01`; // Birth of the Internet
 
 const supportsInputMonth = (() => {
   try {
@@ -67,7 +69,9 @@ function AccountStatuses() {
   }, [sameCurrentInstance, account?.acct]);
 
   async function fetchAccountStatuses(firstLoad) {
-    if (/^\d{4}-[01]\d$/.test(month)) {
+    const isValidMonth = /^\d{4}-[01]\d$/.test(month);
+    const isValidYear = month?.split?.('-')?.[0] >= MIN_YEAR;
+    if (isValidMonth && isValidYear) {
       if (!account) {
         return {
           value: [],
@@ -297,31 +301,33 @@ function AccountStatuses() {
           ))}
           {searchEnabled &&
             (supportsInputMonth ? (
-              <input
-                type="month"
-                class={`filter-field ${month ? 'is-active' : ''}`}
-                disabled={!account?.acct}
-                value={month || ''}
-                min="1983-01" // Birth of the Internet
-                max={new Date().toISOString().slice(0, 7)}
-                onInput={(e) => {
-                  const { value } = e.currentTarget;
-                  setSearchParams(
-                    value
-                      ? {
-                          month: value,
-                        }
-                      : {},
-                  );
-                }}
-              />
+              <label class={`filter-field ${month ? 'is-active' : ''}`}>
+                <Icon icon="month" size="l" />
+                <input
+                  type="month"
+                  disabled={!account?.acct}
+                  value={month || ''}
+                  min={MIN_YEAR_MONTH}
+                  max={new Date().toISOString().slice(0, 7)}
+                  onInput={(e) => {
+                    const { value } = e.currentTarget;
+                    setSearchParams(
+                      value
+                        ? {
+                            month: value,
+                          }
+                        : {},
+                    );
+                  }}
+                />
+              </label>
             ) : (
               // Fallback to <select> for month and <input type="number"> for year
               <MonthPicker
                 class={`filter-field ${month ? 'is-active' : ''}`}
                 disabled={!account?.acct}
                 value={month || ''}
-                min="1983-01" // Birth of the Internet
+                min={MIN_YEAR_MONTH}
                 max={new Date().toISOString().slice(0, 7)}
                 onInput={(e) => {
                   const { value } = e;
@@ -465,6 +471,7 @@ function MonthPicker(props) {
 
   return (
     <div class={className}>
+      <Icon icon="month" size="l" />
       <select
         ref={monthFieldRef}
         disabled={disabled}
@@ -497,7 +504,7 @@ function MonthPicker(props) {
         type="number"
         disabled={disabled}
         value={_year || new Date().getFullYear()}
-        min={min?.slice(0, 4) || '1983'}
+        min={min?.slice(0, 4) || MIN_YEAR}
         max={max?.slice(0, 4) || new Date().getFullYear()}
         onInput={(e) => {
           const { value } = e.currentTarget;

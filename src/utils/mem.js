2c6d18bcfcb43a670651f8254e35719e1d8822ed
Lim Chee Aun <cheeaun@gmail.com>
2023-11-04 19:19:42 +0800

    Reduce to 50

--- src/utils/mem.js
+++ src/utils/mem.js
@@ -3,5 +3,5 @@ import moize from 'moize';
 window._moize = moize;
 
 export default function mem(fn, opts = {}) {
-  return moize(fn, { ...opts, maxSize: 100, isDeepEqual: true });
+  return moize(fn, { ...opts, maxSize: 50, isDeepEqual: true });
 }

25e13144a36f4d8fa76384ff017f706d4b0739bd
Lim Chee Aun <cheeaun@gmail.com>
2023-11-25 21:22:51 +0800

    s/Calckey/Firefish

--- src/utils/isMastodonLinkMaybe.jsx
+++ src/utils/isMastodonLinkMaybe.jsx
@@ -3,7 +3,7 @@ export default function isMastodonLinkMaybe(url) {
   return (
     /^\/.*\/\d+$/i.test(pathname) ||
     /^\/@[^/]+\/(statuses|posts)\/\w+\/?$/i.test(pathname) || // GoToSocial, Takahe
-    /^\/notes\/[a-z0-9]+$/i.test(pathname) || // Misskey, Calckey
+    /^\/notes\/[a-z0-9]+$/i.test(pathname) || // Misskey, Firefish
     /^\/notes\/[a-z0-9]+$/i.test(pathname) || // Misskey, Calckey
     /^\/(notice|objects)\/[a-z0-9-]+$/i.test(pathname) || // Pleroma
     /#\/[^\/]+\.[^\/]+\/s\/.+/i.test(hash) // Phanpy 🫣

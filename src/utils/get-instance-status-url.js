7019c09e5be5e00142d96271d3f5d3f72c363b79
Lim Chee Aun <cheeaun@gmail.com>
2023-11-25 21:26:27 +0800

    Better resolving of links

--- src/utils/get-instance-status-url.js
+++ src/utils/get-instance-status-url.js
@@ -1,19 +1,36 @@
-export const statusRegex = /\/@([^@\/]+)@?([^\/]+)?\/([^\/]+)\/?$/i;
-export const statusNoteRegex = /\/notes\/([^\/]+)\/?$/i;
-function getInstanceStatusURL(url) {
+// export const statusRegex = /\/@([^@\/]+)@?([^\/]+)?\/([^\/]+)\/?$/i;
+// export const statusNoteRegex = /\/notes\/([^\/]+)\/?$/i;
+
+const statusPostRegexes = [
+  /^\/@[^@\/]+\/(?:statuses|posts)\/([^\/]+)/i, // GoToSocial, Takahe
+  /\/notes\/([^\/]+)/i, // Misskey, Firefish
+  /^\/(?:notice|objects)\/([a-z0-9-]+)/i, // Pleroma
+  /\/@[^@\/]+@?[^\/]+?\/([^\/]+)/i, // Mastodon
+];
+
+export function getInstanceStatusObject(url) {
   // Regex /:username/:id, where username = @username or @username@domain, id = anything
   const { hostname, pathname } = new URL(url);
-  const [, username, domain, id] = pathname.match(statusRegex) || [];
-
-  if (id) {
-    return `/${hostname}/s/${id}`;
+  // const [, username, domain, id] = pathname.match(statusRegex) || [];
+  for (const regex of statusPostRegexes) {
+    const [, id] = pathname.match(regex) || [];
+    console.log(pathname, regex, id);
+    if (id) {
+      return {
+        instance: hostname,
+        id,
+      };
+    }
   }
+  return null;
+}
 
-  const [, noteId] = pathname.match(statusNoteRegex) || [];
-
-  if (noteId) {
-    return `/${hostname}/s/${noteId}`;
+function getInstanceStatusURL(url) {
+  const { instance, id } = getInstanceStatusObject(url);
+  if (instance && id) {
+    return `/${instance}/s/${id}`;
   }
+  return null;
 }
 
 export default getInstanceStatusURL;

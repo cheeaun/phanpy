d7d838ebf8bc79bc199a5ecfe31cb9f4c52a8c2f
Lim Chee Aun <cheeaun@gmail.com>
2023-12-29 18:29:08 +0800

    Rebuild useScroll, less states

---
+++ src/utils/useScrollFn.js
@@ -0,0 +1,126 @@
+import { useEffect, useLayoutEffect, useState } from 'preact/hooks';
+
+export default function useScrollFn(
+  {
+    scrollableRef,
+    distanceFromStart = 1, // ratio of clientHeight/clientWidth
+    distanceFromEnd = 1, // ratio of clientHeight/clientWidth
+    scrollThresholdStart = 10,
+    scrollThresholdEnd = 10,
+    direction = 'vertical',
+    distanceFromStartPx: _distanceFromStartPx,
+    distanceFromEndPx: _distanceFromEndPx,
+    init,
+  } = {},
+  callback,
+  deps,
+) {
+  if (!callback) return;
+  const [scrollDirection, setScrollDirection] = useState(null);
+  const [reachStart, setReachStart] = useState(false);
+  const [reachEnd, setReachEnd] = useState(false);
+  const [nearReachStart, setNearReachStart] = useState(false);
+  const [nearReachEnd, setNearReachEnd] = useState(false);
+  const isVertical = direction === 'vertical';
+
+  useLayoutEffect(() => {
+    const scrollableElement = scrollableRef.current;
+    if (!scrollableElement) return {};
+    let previousScrollStart = isVertical
+      ? scrollableElement.scrollTop
+      : scrollableElement.scrollLeft;
+
+    function onScroll() {
+      const {
+        scrollTop,
+        scrollLeft,
+        scrollHeight,
+        scrollWidth,
+        clientHeight,
+        clientWidth,
+      } = scrollableElement;
+      const scrollStart = isVertical ? scrollTop : scrollLeft;
+      const scrollDimension = isVertical ? scrollHeight : scrollWidth;
+      const clientDimension = isVertical ? clientHeight : clientWidth;
+      const scrollDistance = Math.abs(scrollStart - previousScrollStart);
+      const distanceFromStartPx =
+        _distanceFromStartPx ||
+        Math.min(
+          clientDimension * distanceFromStart,
+          scrollDimension,
+          scrollStart,
+        );
+      const distanceFromEndPx =
+        _distanceFromEndPx ||
+        Math.min(
+          clientDimension * distanceFromEnd,
+          scrollDimension,
+          scrollDimension - scrollStart - clientDimension,
+        );
+
+      if (
+        scrollDistance >=
+        (previousScrollStart < scrollStart
+          ? scrollThresholdEnd
+          : scrollThresholdStart)
+      ) {
+        setScrollDirection(previousScrollStart < scrollStart ? 'end' : 'start');
+        previousScrollStart = scrollStart;
+      }
+
+      setReachStart(scrollStart <= 0);
+      setReachEnd(scrollStart + clientDimension >= scrollDimension);
+      setNearReachStart(scrollStart <= distanceFromStartPx);
+      setNearReachEnd(
+        scrollStart + clientDimension >= scrollDimension - distanceFromEndPx,
+      );
+    }
+
+    scrollableElement.addEventListener('scroll', onScroll, { passive: true });
+
+    return () => scrollableElement.removeEventListener('scroll', onScroll);
+  }, [
+    distanceFromStart,
+    distanceFromEnd,
+    scrollThresholdStart,
+    scrollThresholdEnd,
+  ]);
+
+  useEffect(() => {
+    callback({
+      scrollDirection,
+      reachStart,
+      reachEnd,
+      nearReachStart,
+      nearReachEnd,
+    });
+  }, [
+    scrollDirection,
+    reachStart,
+    reachEnd,
+    nearReachStart,
+    nearReachEnd,
+    ...deps,
+  ]);
+
+  useEffect(() => {
+    if (init && scrollableRef.current) {
+      queueMicrotask(() => {
+        scrollableRef.current.dispatchEvent(new Event('scroll'));
+      });
+    }
+  }, [init]);
+
+  // return {
+  //   scrollDirection,
+  //   reachStart,
+  //   reachEnd,
+  //   nearReachStart,
+  //   nearReachEnd,
+  //   init: () => {
+  //     if (scrollableRef.current) {
+  //       scrollableRef.current.dispatchEvent(new Event('scroll'));
+  //     }
+  //   },
+  // };
+}

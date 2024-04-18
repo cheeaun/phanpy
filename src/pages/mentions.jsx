9bf77fa97a3fce53e06dc878d9e66c5ded3107fe
Lim Chee Aun <cheeaun@gmail.com>
2024-04-18 17:15:51 +0800

    Mentions also need fixNotifications
    It's also from notifications API

--- src/pages/mentions.jsx
+++ src/pages/mentions.jsx
@@ -4,6 +4,7 @@ import { useSearchParams } from 'react-router-dom';
 import Link from '../components/link';
 import Timeline from '../components/timeline';
 import { api } from '../utils/api';
+import { fixNotifications } from '../utils/group-notifications';
 import { saveStatus } from '../utils/states';
 import useTitle from '../utils/useTitle';
 
@@ -30,6 +31,8 @@ function Mentions({ columnMode, ...props }) {
     const results = await mentionsIterator.current.next();
     let { value } = results;
     if (value?.length) {
+      value = fixNotifications(value);
+
       if (firstLoad) {
         latestItem.current = value[0].id;
         console.log('First load', latestItem.current);

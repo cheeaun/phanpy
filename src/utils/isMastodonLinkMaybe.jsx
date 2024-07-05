a72400febf62e34e0aaaeef86ae64ed6e373d492
Lim Chee Aun <cheeaun@gmail.com>
2024-07-05 16:19:04 +0800

    Test support Hollo

--- src/utils/isMastodonLinkMaybe.jsx
+++ src/utils/isMastodonLinkMaybe.jsx
@@ -7,6 +7,7 @@ export default function isMastodonLinkMaybe(url) {
       /^\/notes\/[a-z0-9]+$/i.test(pathname) || // Misskey, Firefish
       /^\/(notice|objects)\/[a-z0-9-]+$/i.test(pathname) || // Pleroma
       /^\/@[^/]+\/post\/[a-z0-9]+$/i.test(pathname) || // Threads
+      /^\/@[^/]+\/[a-z0-9]+[a-z0-9\-]+[a-z0-9]+$/i.test(pathname) || // Hollo
       /#\/[^\/]+\.[^\/]+\/s\/.+/i.test(hash) // Phanpy 🫣
     );
   } catch (e) {

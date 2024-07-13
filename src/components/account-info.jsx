44d440649f7c3e14976144f30a49cc11f88e0fb3
Scott Feeney <scott@oceanbase.org>
2024-07-13 01:15:01 -0700

    Include domain when copying local user's handle
    Fixes #596

--- src/components/account-info.jsx
+++ src/components/account-info.jsx
@@ -568,9 +568,9 @@ function AccountInfo({
                   </div>
                   <MenuItem
                     onClick={() => {
-                      const handle = `@${acct}`;
+                      const handleWithInstance = acct.includes('@') ? `@${acct}` : `@${acct}@${instance}`;
                       try {
-                        navigator.clipboard.writeText(handle);
+                        navigator.clipboard.writeText(handleWithInstance);
                         showToast('Handle copied');
                       } catch (e) {
                         console.error(e);
@@ -924,6 +924,8 @@ function RelatedActions({
   const [currentInfo, setCurrentInfo] = useState(null);
   const [isSelf, setIsSelf] = useState(false);
 
+  const acctWithInstance = acct.includes('@') ? acct : `${acct}@${instance}`;
+
   useEffect(() => {
     if (info) {
       const currentAccount = getCurrentAccountID();
@@ -1205,7 +1207,7 @@ function RelatedActions({
             )}
             <MenuItem
               onClick={() => {
-                const handle = `@${currentInfo?.acct || acct}`;
+                const handle = `@${currentInfo?.acct || acctWithInstance}`;
                 try {
                   navigator.clipboard.writeText(handle);
                   showToast('Handle copied');
@@ -1220,7 +1222,7 @@ function RelatedActions({
                 Copy handle
                 <br />
                 <span class="more-insignificant">
-                  @{currentInfo?.acct || acct}
+                  @{currentInfo?.acct || acctWithInstance}
                 </span>
               </small>
             </MenuItem>

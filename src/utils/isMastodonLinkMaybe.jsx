925319b8bb82e9da21c6f4d7907554f5c46a30c2
Lim Chee Aun <cheeaun@gmail.com>
2024-09-02 21:00:10 +0800

    Fix Threads url regex

--- src/utils/isMastodonLinkMaybe.jsx
+++ src/utils/isMastodonLinkMaybe.jsx
@@ -6,7 +6,7 @@ export default function isMastodonLinkMaybe(url) {
       /^\/(@[^/]+|users\/[^/]+)\/(statuses|posts)\/\w+\/?$/i.test(pathname) || // GoToSocial, Takahe
       /^\/notes\/[a-z0-9]+$/i.test(pathname) || // Misskey, Firefish
       /^\/(notice|objects)\/[a-z0-9-]+$/i.test(pathname) || // Pleroma
-      /^\/@[^/]+\/post\/[a-z0-9]+$/i.test(pathname) || // Threads
+      /^\/@[^/]+\/post\/[a-z0-9\-_]+$/i.test(pathname) || // Threads
       /^\/@[^/]+\/[a-z0-9]+[a-z0-9\-]+[a-z0-9]+$/i.test(pathname) || // Hollo
       (hostname === 'fed.brid.gy' && pathname.startsWith('/r/http')) || // Bridgy Fed
       /#\/[^\/]+\.[^\/]+\/s\/.+/i.test(hash) // Phanpy 🫣

a09651e89249aa741cce5238839362e8b75f275e
Stefano Pigozzi <me@steffo.eu>
2024-09-23 03:58:31 +0200

    Revert "Hide trending if not compatible"
    This reverts commit 324384a9a161538c438324b150fb3b8d92507462.

--- src/components/nav-menu.jsx
+++ src/components/nav-menu.jsx
@@ -374,14 +374,12 @@ function NavMenu(props) {
               <Trans>Search</Trans>
             </span>
           </MenuLink>
-          {(supports("@mastodon/trending-hashtags") || supports("@mastodon/trending-links") || supports("@pixelfed/trending")) &&
-            <MenuLink to={`/${instance}/trending`}>
-              <Icon icon="chart" size="l" />{' '}
-              <span>
-                <Trans>Trending</Trans>
-              </span>
-            </MenuLink>
-          }
+          <MenuLink to={`/${instance}/trending`}>
+            <Icon icon="chart" size="l" />{' '}
+            <span>
+              <Trans>Trending</Trans>
+            </span>
+          </MenuLink>
           <MenuLink to={`/${instance}/p/l`}>
             <Icon icon="building" size="l" />{' '}
             <span>

75556b37ccb2960a465157632fc86c10434b79cd
Lim Chee Aun <cheeaun@gmail.com>
2025-04-20 08:11:00 +0800

    Move percentage threshold to build time
    Once listed, always listed

--- src/locales.js
+++ src/locales.js
@@ -3,22 +3,15 @@ import catalogs from './data/catalogs.json';
 export const DEFAULT_LANG = 'en';
 export const CATALOGS = catalogs;
 
-// Get locales that's >= X% translated
-const PERCENTAGE_THRESHOLD = 50;
-
 const locales = [
   DEFAULT_LANG,
-  ...catalogs
-    .filter(({ completion }) => completion >= PERCENTAGE_THRESHOLD)
-    .map(({ code }) => code),
+  ...catalogs.filter(({ listed }) => listed).map(({ code }) => code),
 ];
 export const LOCALES = locales;
 
 let devLocales = [];
 if (import.meta.env?.DEV || import.meta.env?.PHANPY_SHOW_DEV_LOCALES) {
-  devLocales = catalogs
-    .filter(({ completion }) => completion < PERCENTAGE_THRESHOLD)
-    .map(({ code }) => code);
+  devLocales = catalogs.filter(({ listed }) => !listed).map(({ code }) => code);
   devLocales.push('pseudo-LOCALE');
 }
 export const DEV_LOCALES = devLocales;

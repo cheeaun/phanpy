a7942f53a8eb517e22e402cfaf4221a9aefbddab
Lim Chee Aun <cheeaun@gmail.com>
2025-11-12 16:32:02 +0800

    Sort imports

--- src/components/qr-code-modal.jsx
+++ src/components/qr-code-modal.jsx
@@ -2,9 +2,10 @@ import './qr-code-modal.css';
 
 import { useLingui } from '@lingui/react/macro';
 
+import states from '../utils/states';
+
 import Icon from './icon';
 import QrCode from './qr-code';
-import states from '../utils/states';
 
 function QrCodeModal({ text, arena, backgroundMask, caption, onClose }) {
   const { t } = useLingui();

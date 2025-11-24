27ce5bc30a7bb79cdfefcb0eafb84fb056628430
Lim Chee Aun <cheeaun@gmail.com>
2025-11-24 23:45:36 +0800

    Move menu items into a submenu

--- src/components/status.jsx
+++ src/components/status.jsx
@@ -29,6 +29,7 @@ import { isFiltered } from '../utils/filters';
 import getTranslateTargetLanguage from '../utils/get-translate-target-language';
 import getHTMLText from '../utils/getHTMLText';
 import htmlContentLength from '../utils/html-content-length';
+import isRTL from '../utils/is-rtl';
 import localeMatch from '../utils/locale-match';
 import mem from '../utils/mem';
 import niceDateTime from '../utils/nice-date-time';
@@ -1384,18 +1385,31 @@ function Status({
           </MenuItem>
         </>
       )}
-      <MenuItem href={url} target="_blank">
-        <Icon icon="external" />
-        <small
-          class="menu-double-lines should-cloak"
-          style={{
-            maxWidth: '16em',
-          }}
-        >
-          {nicePostURL(url)}
-        </small>
-      </MenuItem>
-      <div class="menu-horizontal">
+      <SubMenu2
+        direction={isRTL() ? 'left' : 'right'}
+        overflow="auto"
+        gap={-8}
+        label={
+          <>
+            <Icon icon="link" />
+            <small
+              class="menu-double-lines should-cloak"
+              style={{
+                maxWidth: '16em',
+              }}
+            >
+              {nicePostURL(url)}
+            </small>
+            <Icon icon="chevron-right" />
+          </>
+        }
+      >
+        <MenuItem href={url} target="_blank">
+          <Icon icon="external" />
+          <span>
+            <Trans>Open link</Trans>
+          </span>
+        </MenuItem>
         <MenuItem
           onClick={() => {
             // Copy url to clipboard
@@ -1408,7 +1422,7 @@ function Status({
             }
           }}
         >
-          <Icon icon="link" />
+          <Icon icon="copy" />
           <span>
             <Trans>Copy</Trans>
           </span>
@@ -1436,7 +1450,7 @@ function Status({
               </span>
             </MenuItem>
           )}
-      </div>
+      </SubMenu2>
       {isPublic && isSizeLarge && (
         <MenuItem
           onClick={() => {

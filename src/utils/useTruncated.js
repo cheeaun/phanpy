fc21dcf56c5f721a93b16afca03dd99060f244e2
Lim Chee Aun <cheeaun@gmail.com>
2026-02-20 09:05:41 +0800

    Revert to previous height-only check
    The width check is not reliable and to fix it, it needs more research.

--- src/utils/useTruncated.js
+++ src/utils/useTruncated.js
@@ -1,4 +1,4 @@
-import { useCallback, useRef } from 'preact/hooks';
+import { useRef } from 'preact/hooks';
 
 import useThrottledResizeObserver from './useThrottledResizeObserver';
 
@@ -8,36 +8,25 @@ export default function useTruncated({
 } = {}) {
   const ref = useRef();
   const prevTruncatedRef = useRef();
-  const onResize = useCallback(
-    ({ height, width }) => {
-      if (ref.current) {
-        const { scrollHeight, scrollWidth } = ref.current;
-        let truncated = scrollHeight > height || scrollWidth > width;
-        if (truncated) {
-          const {
-            height: _height,
-            maxHeight,
-            width: _width,
-            maxWidth,
-          } = getComputedStyle(ref.current);
-          const computedHeight =
-            parseInt(maxHeight, 10) || parseInt(_height, 10);
-          const computedWidth = parseInt(maxWidth, 10) || parseInt(_width, 10);
-          truncated =
-            scrollHeight > computedHeight || scrollWidth > computedWidth;
-        }
-        ref.current.classList.toggle(className, truncated);
-        if (
-          prevTruncatedRef.current !== truncated &&
-          typeof onTruncated === 'function'
-        ) {
-          prevTruncatedRef.current = truncated;
-          onTruncated(truncated);
-        }
+  const onResize = ({ height }) => {
+    if (ref.current) {
+      const { scrollHeight } = ref.current;
+      let truncated = scrollHeight > height;
+      if (truncated) {
+        const { height: _height, maxHeight } = getComputedStyle(ref.current);
+        const computedHeight = parseInt(maxHeight || _height, 10);
+        truncated = scrollHeight > computedHeight;
       }
-    },
-    [className, onTruncated],
-  );
+      ref.current.classList.toggle(className, truncated);
+      if (
+        prevTruncatedRef.current !== truncated &&
+        typeof onTruncated === 'function'
+      ) {
+        prevTruncatedRef.current = truncated;
+        onTruncated(truncated);
+      }
+    }
+  };
   useThrottledResizeObserver({
     ref,
     box: 'border-box',

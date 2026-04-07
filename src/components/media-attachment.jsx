296910499bfd93915010e42d570346ed2420d2c3
Lim Chee Aun <cheeaun@gmail.com>
2026-04-07 19:22:31 +0800

    Fix description generation not working

--- src/components/media-attachment.jsx
+++ src/components/media-attachment.jsx
@@ -473,7 +473,10 @@ function MediaAttachment({
                             (async function () {
                               try {
                                 const body = new FormData();
-                                body.append('image', file);
+                                const fileObj = fileData
+                                  ? new File([fileData], fileName || 'upload', { type })
+                                  : file;
+                                body.append('image', fileObj);
                                 const response = await fetch(IMG_ALT_API_URL, {
                                   method: 'POST',
                                   body,
@@ -522,7 +525,10 @@ function MediaAttachment({
                               (async function () {
                                 try {
                                   const body = new FormData();
-                                  body.append('image', file);
+                                  const fileObj = fileData
+                                    ? new File([fileData], fileName || 'upload', { type })
+                                    : file;
+                                  body.append('image', fileObj);
                                   const params = `?lang=${lang}`;
                                   const response = await fetch(
                                     IMG_ALT_API_URL + params,
